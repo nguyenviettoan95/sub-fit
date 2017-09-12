254b6a086156f3e446e7410594a93ecd766adfec
nguyenviettoan95 <30643631+nguyenviettoan95@users.noreply.github.com>
2017-09-12 15:38:48 +0700

    add some subpage

---
+++ viennang.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>SUB-FIT</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<div class="main">
+	<div class="container">
+		 <div class="navbar-header visible-xs">
+		  	<div class="panel panel-default btn">
+		  		<i class="fa fa-list-ul" aria-hidden="true"></i>
+		  </div>
+		  	</div>
+		       
+	   
+	    <div class='logo'>
+	    	<img src="img/logo.png">
+	    </div>
+	</div>
+	<div class='menu collapse navbar-collapse' id="bs-example-navbar-collapse-1">
+		<div class="container">
+			<nav class="navbar navbar-default " role="navigation">
+				<ul class="nav navbar-nav">
+					<li><a href="#">Viên Nang Giảm Cân FIT</a></li>
+					<li><a href="kienthuc.php">Kiến Thức Giảm Cân</a></li>
+					<li><a href="hoidap.php">Hỏi Đáp FIT</a></li>
+					<li><a href="#">Hotline: 0944 223 224</a></li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+	<div class="content">
+		<div class='container'>
+			<div class='row'>
+				<div class='col-md-8'>
+					<div class="panel panel-default">
+						<div class="content1">
+							<div class='product'>
+								<img src="img/product.jpg" style=" width: 100%;">
+							</div>
+							<div class="content1-1">
+								<div class="head1">
+									<h2><strong>Why do we use it?</strong></h2>
+								</div>
+								<div class="info">
+									<div class="time">
+									<p><i class="fa fa-clock-o" aria-hidden="true"></i> <a href="#">01/08/2017</a></p>
+									</div>
+									<div class="folder">
+										<p><i class="fa fa-folder-o" aria-hidden="true"></i> <a href="#">Hỏi Đáp FIT</a>,<a> Kiến Thức Giảm Cân</a> </p>
+									</div>
+								</div>
+								<div class="clearfix"></div>
+								<div class="describle">
+									<p>Lorem Ipsum “Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit…” “There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain…” What is…</p>
+								</div>
+								
+							</div>
+
+						</div>
+						<div class="content1">
+							<div class='product'>
+								<img src="img/product.jpg" style=" width: 100%;">
+							</div>
+							<div class="content1-1">
+								<div class="head1">
+									<h2><strong>Where does it come from?</strong></h2>
+								</div>
+								<div class="info">
+									<div class="time">
+									<p><i class="fa fa-clock-o" aria-hidden="true"></i> <a href="#">01/08/2017</a></p>
+									</div>
+									<div class="folder">
+										<p><i class="fa fa-folder-o" aria-hidden="true"></i> <a href="#">Hỏi Đáp FIT</a>,<a> Kiến Thức Giảm Cân</a> </p>
+									</div>
+								</div>
+								<div class="clearfix"></div>
+								<div class="describle">
+									<p>Lorem Ipsum “Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit…” “There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain…” What is…</p>
+								</div>
+								
+							</div>
+							
+						</div>
+						<div class="content1">
+							<div class='product'>
+								<img src="img/product.jpg" style=" width: 100%;">
+							</div>
+							<div class="content1-1">
+								<div class="head1">
+									<h2><strong>What is Lorem Ipsum?</strong></h2>
+								</div>
+								<div class="info">
+									<div class="time">
+									<p><i class="fa fa-clock-o" aria-hidden="true"></i> <a href="#">01/08/2017</a></p>
+									</div>
+									<div class="folder">
+										<p><i class="fa fa-folder-o" aria-hidden="true"></i> <a href="#">Hỏi Đáp FIT</a>,<a> Kiến Thức Giảm Cân</a> </p>
+									</div>
+								</div>
+								<div class="clearfix"></div>
+								<div class="describle">
+									<p>Lorem Ipsum “Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit…” “There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain…” What is…</p>
+								</div>
+								
+							</div>
+							
+						</div>
+						<div class="content1">
+							<div class='product'>
+								<img src="img/product.jpg" style=" width: 100%;">
+							</div>
+							<div class="content1-1">
+								<div class="head1">
+									<h2><strong>Hello World!</strong></h2>
+								</div>
+								<div class="info">
+									<div class="time">
+									<p><i class="fa fa-clock-o" aria-hidden="true"></i> <a href="#">01/08/2017</a></p>
+									</div>
+									<div class="folder">
+										<p><i class="fa fa-folder-o" aria-hidden="true"></i> <a href="#">Hỏi Đáp FIT</a>,<a> Kiến Thức Giảm Cân</a> </p>
+									</div>
+								</div>
+								<div class="clearfix"></div>
+								<div class="describle">
+									<p>Lorem Ipsum “Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit…” “There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain…” What is…</p>
+								</div>
+								
+							</div>
+							
+						</div>
+					</div>
+				</div>
+				<div class='col-md-4'>
+					<div class="panel panel-default">
+						<div class="search">
+							<h2>Tìm Kiếm</h2>
+							<div class="progress">
+							    <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:20%">
+							      <span class="sr-only"></span>
+							    </div>
+							 </div>
+							<div class="clearfix"></div>
+							<form class="search-form">
+								<div class="input-group">
+							    <input type="text" class="form-control" style="    border-radius: 0px;" placeholder="Search...">
+							    <div class="input-group-btn" style="background-color: #872123;">
+							      <button class="btn btn-default" style="    background-color: #872123;border: none !important	;" type="submit">
+							        <i class="glyphicon glyphicon-search"></i>
+							      </button>
+							    </div>
+							</div>
+							</form>
+							
+						</div>	
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="panel panel-default">
+						<div class="post">
+							<h2>Bài Viết Mới Nhất</h2>
+							<div class="progress">
+							    <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:20%">
+							      <span class="sr-only"></span>
+							    </div>
+							 </div>
+							<div class="clearfix"></div>
+							<div class="article">
+								<ul>
+								<li><i class="fa fa-tags" aria-hidden="true"></i><a href="#"> Why do we use it?</a> 01/08/2017</li>
+								<li><i class="fa fa-tags" aria-hidden="true"></i><a href="#"> Why does it come from?</a> 01/08/2017</li>
+								<li><i class="fa fa-tags" aria-hidden="true"></i><a href="#"> What is Lorem Ipsum?</a> 01/08/2017</li>
+								<li><i class="fa fa-tags" aria-hidden="true"></i><a href="#"> Hello world!</a> 01/08/2017</li>
+							</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<footer>
+	<div id="container">
+		<div class='menu'>
+			<div class="container">
+				<nav class="navbar navbar-default2 " role="navigation">
+					<ul class="nav navbar-nav">
+						<li><a href="viennang.php">Viên Nang Giảm Cân FIT</a></li>
+						<li><a href="kienthuc.php">Kiến Thức Giảm Cân</a></li>
+						<li><a href="hoidap.php">Hỏi Đáp FIT</a></li>
+						<li><a href="#">Hotline: 0944 223 224</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+		<hr>
+		<div class="copy">
+				Copyright © All rights reserved.
+		</div>
+	</div>
+	</footer>
+</div>
+ <div id="slidebar">
+	    	<ul>
+	    		<li><a href="viennang.php">Viên Nang Giảm Cân FIT</a></li>
+	    		<li><a href="kienthuc.php">Kiến Thức Giảm Cân</a></li>
+	    		<li><a href="hoidap.php">Hỏi Đáp FIT</a></li>
+	    		<li><a href="#">Hotline: 0944 223 224</a></li>
+	    	</ul>
+	    </div>
+	<div id="2top" style="position: fixed; right: 15px;bottom: 20px; cursor: pointer;">
+		<div class="panel panel-default" style="    background-color: #872123;    padding: 5px 10px;right: 15px;">
+			<div class="glyphicon glyphicon-chevron-up" style=" padding-left: 2px;color: white;">
+				
+			</div>
+		</div>
+	</div>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script>
+		$(function(){
+			$(window).scroll(function(){
+			if($(this).scrollTop()>100){
+				$('#2top').fadeIn();
+			}
+			else{
+				$('#2top').fadeOut();
+			}
+			});
+			$('#2top').click(function(){
+				$('html,body').animate({scrollTop:0},300);
+				return false;
+			});
+		});
+	</script>
+	<script>
+		$(document).ready(function(){
+			$('.navbar-header').click(function(){
+				$('#slidebar').toggleClass('menuleft');
+				$('.main').toggleClass('move');
+			});
+		});
+	</script>
+</body>
+</html>
